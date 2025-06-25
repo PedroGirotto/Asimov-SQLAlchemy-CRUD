d2d38e89920a1096307f2caa568984a27a599872
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-06-25 09:28:29 -0300

    Criando as classes responsáveis pelas tabelas no banco de dados Postgre

---
+++ src/Usuario.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+import Base
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(30))
+    senha: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
+    acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
+
+    def __repr__(self):
+        return f'Usuario({self.id=}, {self.nome})'
