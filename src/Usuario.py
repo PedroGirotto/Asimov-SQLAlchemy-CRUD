1a77a76275d254ede39699904698710cd3309126
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-06-25 18:29:01 -0300

    Adicionando um construtor na classe Usuario

--- src/Usuario.py
+++ src/Usuario.py
@@ -11,5 +11,12 @@ class Usuario(Base):
     email: Mapped[str] = mapped_column(String(30))
     acesso_gestor: Mapped[bool] = mapped_column(Boolean(), default=False)
 
+    def __init__(self, id, nome, senha, email, acesso_gestor):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+        self.email = email
+        self.acesso_gestor = acesso_gestor
+
     def __repr__(self):
-        return f'Usuario({self.id=}, {self.nome}=)'
+        return f'Usuario({self.id=}, {self.nome=})'
