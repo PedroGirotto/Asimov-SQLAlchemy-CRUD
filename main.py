7c3c1e47ba27c9c54e724e8d4941f8a879b624e8
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-07-01 13:15:46 -0300

    Reorganizando o main.py para inicializar o FrontEnd

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import os
 from dotenv import load_dotenv
 
 from sqlalchemy import create_engine, URL
-from src.Usuario import *
-from src.Base import Base
+
+from src.Usuario import * # Base é importado em Usuario
+from src.Frontend import *
 
 load_dotenv()
 
 url_object = URL.create(
-    drivername = 'postgresql+psycopg2',
-    username = os.getenv('LOGIN'),
-    password = os.getenv('PASSWORD'),
-    host = os.getenv('HOST'),
-    port = int(os.getenv('PORT'), 10), # type: ignore
-    database = os.getenv('DATABASE')
+    drivername = "postgresql+psycopg2",
+    username = os.getenv("LOGIN"),
+    password = os.getenv("PASSWORD"),
+    host = os.getenv("HOST"),
+    port = int(os.getenv("PORT"), 10), # type: ignore
+    database = os.getenv("DATABASE")
 )
 
 
@@ -21,22 +22,5 @@ engine = create_engine(url_object, echo=False)
 Base.metadata.create_all(bind=engine)
 
 
-if __name__ == '__main__':
-    # teste básico das funções
-
-    # criar_usuario(
-    #     engine,
-    #     nome='Leonardo Maia',
-    #     senha='mair_leonardo987123',
-    #     email='maia_leonardo@hotmail.com',
-    #     acesso_gestor=False)
-
-    #print(ler_todos_usuarios(engine))
-    print(ler_usuario_id(engine, 3))
-    #modificar_usuario(engine, id=3, email='novo_mail@gmail.com')
-    #print(ler_usuario_id(engine, id=3))
-
-    #deletar_usuario(engine, id=2)
-
-    usuario = ler_usuario_id(engine, id=3)
-    print(f'Status do login: {usuario.verificar_senha('mair_leonardo987123')}')
+if __name__ == "__main__":
+    start_front(engine)
